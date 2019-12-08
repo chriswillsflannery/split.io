cd20ba74c45918893bff0e720987d106bc817e92
Chris Flannery <chriswillsflannery@gmail.com>
2019-12-08 13:30:10 -0500

    added styling to auth

--- client/components/App.js
+++ client/components/App.js
@@ -10,7 +10,7 @@ const StyledOverlay = styled.div`
   height: 500px;
   background-attachment: fixed;
   background-size: cover;
-  bakground-repeat: noRepeat;
+  bakground-repeat: no-repeat;
   z-index: 3;
   margin-top: -450px;
 `;
@@ -27,42 +27,97 @@ const StyledNav = styled.div`
   }
 `;
 
-const bgStyle = {
-  backgroundImage: `url(${Background})`,
-  width: `100%`,
-  height: `500px`,
-  backgroundAttachment: `fixed`,
-  backgroundSize: `cover`,
-  backgroundRepeat: `no-repeat`,
-  zIndex: `1`
-}
+const StyledBG = styled.div`
+  background-image: url(${Background});
+  width: 100%;
+  height: 600px;
+  background-attachment: fixed;
+  background-size: cover;
+  background-repeat: no-repeat;
+  z-index: 1;
+`;
+
+const StyledAuth = styled.div`
+  background-color: #1f4994;
+  opacity: .95;
+  width: 400px;
+  height: 400px;
+  border-radius: 400px;
+  position: absolute;
+  top: 100;
+  left: 50%;
+  z-index: 5;
+`;
+
+const StyledLogin = styled.div`
+  position: absolute;
+  top: 160;
+  left: 58%;
+  z-index: 6;
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    margin: 5px;
+  }
+  input {
+    margin: 5px;
+  }
+  .input {
+    padding: 10px 50px;
+    border-radius: 10px;
+    border: none;
+  }
+  .submit {
+    padding: 10px 30px;
+    border-radius: 10px;
+    border: none;
+    background-color: #d2dbea;
+  }
+`;
+
+const StyledLinks = styled.div`
+  position: absolute;
+  top: 600;
+  left: 70%;
+  z-index: 6;
+  ul {
+    display: flex;
+    list-style-type: none;
+  }
+  a {
+    color: white;
+    margin: 5px;
+  }
+`;
 
 const App = () => {
   return (
     <>
-      {/* <section style={navStyle}>
-        <h2>SPLIT.io</h2>
-      </section> */}
       <StyledNav>
         <h2>Split.io</h2>
       </StyledNav>
-      {/* <section style={overlayStyle} /> */}
       <StyledOverlay />
-      <section style={bgStyle} />
-      <div>
-        <h1>log in:</h1>
+      <StyledBG />
+      <StyledAuth />
+      <StyledLogin>
         <form method="POST" action='/login'>
-          <input name="username" type="text" placeholder="username"></input>
-          <input name="password" type="password" placeholder="password"></input>
-          <input type="submit" value="login"></input>
+          <input className="input" name="username" type="text" placeholder="username"></input>
+          <input className="input" name="password" type="password" placeholder="password"></input>
+          <input className="submit" type="submit" value="login"></input>
         </form>
-        <h1>sign up:</h1>
         <form method="POST" action='/signup'>
-          <input name="username" type="text" placeholder="username"></input>
-          <input name="password" type="password" placeholder="password"></input>
-          <input type="submit" value="sign up"></input>
+          <input className="input" name="username" type="text" placeholder="username"></input>
+          <input className="input" name="password" type="password" placeholder="password"></input>
+          <input className="submit" type="submit" value="sign up"></input>
         </form>
-      </div>
+      </StyledLogin>
+      <StyledLinks>
+        <ul>
+          <li><a href="#">Instagram</a></li>
+          <li><a href="#">Facebook</a></li>
+        </ul>
+      </StyledLinks>
     </>
   )
 }
