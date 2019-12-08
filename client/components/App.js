b27d87eacc5760bcaa0403bdc97e28241bdf1703
Chris Flannery <chriswillsflannery@gmail.com>
2019-12-07 20:18:20 -0500

    started adding styled components

--- client/components/App.js
+++ client/components/App.js
@@ -1,27 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Background from '../../build/images/background.jpg';
 import Overlay from '../../build/images/overlay.png';
 
-const overlayStyle = {
-  backgroundImage: `url(${Overlay})`,
-  width: `100%`,
-  height: `500px`,
-  backgroundAttachment: `fixed`,
-  backgroundSize: `cover`,
-  backgroundRepeat: `no-repeat`,
-  zIndex: `3`,
-  marginTop: `-450`
-}
+const StyledOverlay = styled.div`
+  background-image: url(${Overlay});
+  width: 100%;
+  height: 500px;
+  background-attachment: fixed;
+  background-size: cover;
+  bakground-repeat: noRepeat;
+  z-index: 3;
+  margin-top: -450px;
+`;
 
-const navStyle = {
-  width: `100%`,
-  height: `50px`,
-  position: `fixed`,
-  top: `0`,
-  backgroundColor: `whitesmoke`,
-  zIndex: `-1`,
-}
+const StyledNav = styled.div`
+  width: 100%;
+  height: 50px;
+  position: fixed;
+  top: 0;
+  background-color: whitesmoke;
+  z-index: -1;
+  h2 {
+    margin: 10px 0px 0px 20px;
+  }
+`;
 
 const bgStyle = {
   backgroundImage: `url(${Background})`,
@@ -36,8 +40,14 @@ const bgStyle = {
 const App = () => {
   return (
     <>
-      <section style={navStyle} />
-      <section style={overlayStyle} />
+      {/* <section style={navStyle}>
+        <h2>SPLIT.io</h2>
+      </section> */}
+      <StyledNav>
+        <h2>Split.io</h2>
+      </StyledNav>
+      {/* <section style={overlayStyle} /> */}
+      <StyledOverlay />
       <section style={bgStyle} />
       <div>
         <h1>log in:</h1>
